8300cf89bea6b38ffa299086e6696d50e968f101
Redxnel <redxnel@yandex.ru>
2019-01-25 01:44:25 +0300

    refactoring buildAST

--- src/buildAST.js
+++ src/buildAST.js
@@ -4,20 +4,18 @@ import Node from './Node';
 const buildDiffAST = (data1, data2) => {
   const keys = _.union(Object.keys(data1), Object.keys(data2));
   return keys.map((key) => {
-    if (data1[key] instanceof Object && data2[key] instanceof Object) {
-      return new Node(key, '', 'nested', buildDiffAST(data1[key], data2[key]));
+    switch (true) {
+      case data1[key] instanceof Object && data2[key] instanceof Object:
+        return new Node(key, '', 'nested', buildDiffAST(data1[key], data2[key]));
+      case data1[key] === data2[key]:
+        return new Node(key, data1[key], 'unchanged');
+      case !_.has(data2, key):
+        return new Node(key, data1[key], 'removed');
+      case !_.has(data1, key):
+        return new Node(key, data2[key], 'added');
+      default:
+        return new Node(key, { valueBefore: data1[key], valueAfter: data2[key] }, 'updated');
     }
-    if (data1[key] === data2[key]) {
-      return new Node(key, data1[key], 'unchanged');
-    }
-    if (!_.has(data2, key)) {
-      return new Node(key, data1[key], 'removed');
-    }
-    if (!_.has(data1, key)) {
-      return new Node(key, data2[key], 'added');
-    }
-
-    return new Node(key, { valueBefore: data1[key], valueAfter: data2[key] }, 'updated');
   });
 };
 
