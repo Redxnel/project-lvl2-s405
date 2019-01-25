2067639aac15211e5da44fd7b4ae3f70b8e45914
Redxnel <redxnel@yandex.ru>
2019-01-25 20:49:23 +0300

    refactoring renderPlain, is default case right?

--- src/renderers/renderPlain.js
+++ src/renderers/renderPlain.js
@@ -1,22 +1,38 @@
+import _ from 'lodash';
 import { selectValue } from '../utils';
+
+const propertyActions = [
+  {
+    name: 'added',
+    action: (obj, path) => `Property '${path}${obj.name}' was added with value: ${selectValue(obj.value)}`,
+  },
+  {
+    name: 'updated',
+    action: (obj, path) => `Property '${path}${obj.name}' was updated. From ${selectValue(obj.value.valueBefore)} to ${selectValue(obj.value.valueAfter)}`,
+  },
+  {
+    name: 'removed',
+    action: (obj, path) => `Property '${path}${obj.name}' was removed`,
+  },
+  {
+    name: 'nested',
+    action: (obj, path, func) => func(obj.children, `${path}${obj.name}.`),
+  },
+  {
+    name: 'unchanged',
+    action: () => [],
+  },
+];
+
+const getAction = type => propertyActions.find(({ name }) => name === type);
 
 const render = (ast, name = '') => {
   const result = ast.reduce((acc, node) => {
-    switch (node.type) {
-      case 'added':
-        return [...acc, `Property '${name}${node.name}' was added with value: ${selectValue(node.value)}`];
-      case 'updated':
-        return [...acc, `Property '${name}${node.name}' was updated. From ${selectValue(node.value.valueBefore)} to ${selectValue(node.value.valueAfter)}`];
-      case 'removed':
-        return [...acc, `Property '${name}${node.name}' was removed`];
-      case 'nested':
-        return [...acc, render(node.children, `${name}${node.name}.`)];
-      default:
-        return acc;
-    }
+    const { action } = getAction(node.type);
+    return [...acc, action(node, name, render)];
   }, []);
 
-  return result.join('\n');
+  return _.flatten(result).join('\n');
 };
 
 export default render;
